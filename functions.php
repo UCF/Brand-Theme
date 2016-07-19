49e832f50a7c121f6847b9b41600cf9c7e3b8f86
RJ Bruneel <rj.bruneel@ucf.edu>
2016-07-19 13:20:11 -0400

    Added missing curly brackets

--- functions.php
+++ functions.php
@@ -184,10 +184,11 @@ function get_amazon_url() {
 function display_submenu ( $post ) {
 	ob_start();
 
-	if ( is_page() && $post->post_parent )
+	if ( is_page() && $post->post_parent ) {
 		$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
-	else
+	} else {
 		$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+	}
 
 	if ( $childpages ) {
 		?>
