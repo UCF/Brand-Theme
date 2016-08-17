04de3ab0331d6c3bbfc64fd8ee06ae0ab1c5fadc
RJ Bruneel <rj.bruneel@ucf.edu>
2016-08-17 14:59:50 -0400

    Added mobile menu

--- header.php
+++ header.php
@@ -15,19 +15,30 @@
 				</div>
 			</div>
 			<div class="nav-container container-fluid">
-				<div class="container">
-					<div class="row">
-						<nav class="col-md-12">
-							<?php
-							wp_nav_menu( array(
-								'theme_location' => 'header-menu',
-								'container' => false,
-								'menu_class' => 'nav navbar-nav',
-								'menu_id' => 'mobile-header-menu',
-								'walker' => new Bootstrap_Walker_Nav_Menu()
-							) );
-							?>
-						</nav>
+				<div class="navbar-header">
+					<span class="navbar-title"></span>
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mobile-nav" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse" id="mobile-nav">
+					<div class="container">
+						<div class="row">
+							<nav class="col-md-12">
+								<?php
+								wp_nav_menu( array(
+									'theme_location' => 'header-menu',
+									'container' => false,
+									'menu_class' => 'nav navbar-nav',
+									'menu_id' => 'mobile-header-menu',
+									'walker' => new Bootstrap_Walker_Nav_Menu()
+								) );
+								?>
+							</nav>
+						</div>
 					</div>
 				</div>
 			</div>
