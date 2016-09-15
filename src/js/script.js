61ccf604517eab7ad7d1130b89d656c3d3a3bd91
RJ Bruneel <rj.bruneel@ucf.edu>
2016-09-15 10:37:59 -0400

    Added logic for jump links

--- src/js/script.js
+++ src/js/script.js
@@ -8,35 +8,56 @@ $(function () {
     /**
      * Navigation Affix
      */
-    var $navContainer = $('.nav-container'),
-        offset = $navContainer.offset().top;
+    var $body = $('body'),
+        $navContainer = $('.nav-container'),
+        $jumpGroup = $('.jump-group-container'),
+        offset = $navContainer.offset().top,
+        jumpOffset = $jumpGroup.offset().top - 50;
 
-    var scroll = function() {
-        if ($(window).scrollTop() >= offset) {
+    var scroll = function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop >= offset) {
             $navContainer.addClass('navbar-fixed-top');
-            $('body').addClass('fixed-navbar');
+            $body.addClass('fixed-navbar');
         } else {
             $navContainer.removeClass('navbar-fixed-top');
-            $('body').removeClass('fixed-navbar');
+            $body.removeClass('fixed-navbar');
+        }
+        if (scrollTop >= jumpOffset) {
+            $jumpGroup.addClass('jump-fixed-top');
+            $body.addClass('fixed-jump');
+        } else {
+            $jumpGroup.removeClass('jump-fixed-top');
+            $body.removeClass('fixed-jump');
         }
     };
 
-    var onResize = function() {
+    var onResize = function () {
         offset = $navContainer.offset().top;
+        jumpOffset = $jumpGroup.offset().top;
     };
 
-    var initNavAffix = function() {
+    var initNavAffix = function () {
         $(document).on('scroll', scroll);
-        $('body').scrollspy({ target: '.nav-container' });
+        $body.scrollspy({ target: '.nav-container' });
+        $body.scrollspy({ target: '.jump-group' });
         $(window).on('resize', onResize);
         scroll();
     };
+
+	function smoothScroll(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 120
+        }, 500);
+    }
 
     /**
      * Init
      */
     var init = function () {
         initNavAffix();
+        $('.jump-group').on('click', 'a', smoothScroll);
     };
 
     init();
